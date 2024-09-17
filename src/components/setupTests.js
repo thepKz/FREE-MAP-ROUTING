c32b7db6f642d06a1335243565ec4f74e0553af3
thepKz <thepmtse181568@fpt.edu.vn>
2024-09-18 03:28:14 +0700

    nothing

---
+++ src/components/setupTests.js
@@ -0,0 +1,25 @@
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import React from 'react';
+
+// Mock cho các module CSS
+jest.mock('leaflet/dist/leaflet.css', () => ({}));
+
+// Mock cho các module không JavaScript
+jest.mock('leaflet', () => ({
+  Icon: {
+    Default: {
+      prototype: {
+        _getIconUrl: jest.fn(),
+      },
+      mergeOptions: jest.fn(),
+    },
+  },
+}));
+
+// Thiết lập global mocks
+global.React = React;
